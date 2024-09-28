5859be280e76cb3378e799ffd1c8365c025e2ad3
anisha <gudeanisha@gmail.com>
2024-09-28 05:15:16 +0000

    commit messge

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Game from './component/Game'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Game />
+  </>
+)
+
+export default App
---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+/* eslint-disable */
+
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
